d6483d145e347ee65068ce62bf11b09f158d10ac
Genadi Mihov <gmihov7@gmail.com>
2019-06-02 18:10:53 +0300

    add

---
+++ 03. MultidimensionalArrays-Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jaggedArray = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] nums = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                jaggedArray[i] = nums;
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] tokens = command.Split();
+
+                string operation = tokens[0];
+                int row = int.Parse(tokens[1]);
+                int col = int.Parse(tokens[2]);
+                int value = int.Parse(tokens[3]);
+
+                if (row < 0
+                    || row >= rows
+                    || col < 0
+                    || col >= jaggedArray[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+
+                switch (operation)
+                {
+                    case "Add":
+                        jaggedArray[row][col] += value;
+                        break;
+                    case "Subtract":
+                        jaggedArray[row][col] -= value;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var arr in jaggedArray)
+            {
+                Console.WriteLine(string.Join(' ', arr));
+            }
+
+        }
+    }
+}
---
+++ 03. MultidimensionalArrays-Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _7._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[][] jaggedArr = new int[size][];
+
+            int cols = 1;
+
+            for (int i = 0; i < size; i++)
+            {
+                jaggedArr[i] = new int[cols];
+                jaggedArr[i][0] = 1;
+                jaggedArr[i][cols - 1] = 1;
+
+                if (cols > 2)
+                {
+                    int[] previousRow = jaggedArr[i - 1];
+
+                    for (int j = 0; j < cols - 1; j++)
+                    {
+                        jaggedArr[i][j] = previousRow[j] + previousRow[j - 1];
+                    }
+                }
+
+                cols++;
+            }
+
+            foreach (var item in jaggedArr)
+            {
+                Console.WriteLine(string.Join(' ', item));
+            }
+
+        }
+    }
+}
