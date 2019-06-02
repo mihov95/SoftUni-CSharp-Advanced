04f8234d985ac39e6e4ae662627b5cd052ea165e
Genadi Mihov <gmihov7@gmail.com>
2019-06-02 19:25:23 +0300

    add

---
+++ 03. MultidimensionalArrays-Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                int[] nums = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < size; col++)
+                {
+                    if (row == col)
+                    {
+                        sum += nums[col];
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 03. MultidimensionalArrays-Lab/4. Symbol In Matrix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _4._Symbol_In_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n,n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] rowInput = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row,col] = rowInput[col];
+                }
+            }
+
+            var symbol = Console.ReadLine();
+            var isSymbol = false;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col].ToString() == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        isSymbol = true;
+                        return;
+                    }
+                }
+            }
+
+            if (!isSymbol)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+
+        }
+    }
+}
