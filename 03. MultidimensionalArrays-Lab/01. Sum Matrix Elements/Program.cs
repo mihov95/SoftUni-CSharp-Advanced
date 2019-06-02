dbaadae39be0139be8ce463635bb5c9f2d1f008d
Genadi Mihov <gmihov7@gmail.com>
2019-06-02 16:32:23 +0300

    fixed

--- 03. MultidimensionalArrays-Lab/01. Sum Matrix Elements/Program.cs
+++ 03. MultidimensionalArrays-Lab/01. Sum Matrix Elements/Program.cs
@@ -12,7 +12,9 @@ namespace _01._Sum_Matrix_Elements
                 Select(int.Parse).
                 ToArray();
 
-            int[,] arr = new int[dimensions[0],dimensions[1]];
+            int sum = 0;
+
+            int[,] arr = new int[dimensions[0], dimensions[1]];
 
             for (int i = 0; i < arr.GetLength(0); i++)
             {
@@ -24,22 +26,12 @@ namespace _01._Sum_Matrix_Elements
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
                     arr[i, j] = tokens[j];
+                    sum += arr[i,j];
                 }
             }
 
             Console.WriteLine(arr.GetLength(0));
             Console.WriteLine(arr.GetLength(1));
-
-            int sum = 0;
-
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    sum += arr[i, j];
-                }
-            }
-
             Console.WriteLine(sum);
 
 
