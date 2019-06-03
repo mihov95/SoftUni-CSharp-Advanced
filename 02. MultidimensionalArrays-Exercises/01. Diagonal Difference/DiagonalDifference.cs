f6d933580f9d5e2e087d66bbd8a5e37df2635064
Genadi Mihov <gmihov7@gmail.com>
2019-06-03 21:20:32 +0300

    renamed sln's

---
+++ 02. MultidimensionalArrays-Exercises/01. Diagonal Difference/DiagonalDifference.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Diagonal_Difference
+{
+    class DiagonalDifference
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size,size];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+            }
+
+        }
+    }
+}
